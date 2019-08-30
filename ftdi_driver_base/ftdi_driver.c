f109cbd3e37d1acec06f6c5fff3ca5d269f7aee3
PaulKumar33 <paul.kumar@mail.utoronto.ca>
2019-08-30 15:13:19 -0400

    first commit

---
+++ ftdi_driver_base/ftdi_driver.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h> 
+#include <windows.h>
+#include <windef.h>
+#include <winnt.h>
+#include <winbase.h>
+#include <string.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include "ftd2xx.h"
+#include <sys/time.h>
+
+
+int main(int argc, char * argv[])
+{
+   //code
+	printf("This is the main method\n");
+	printf("press any key to continue\n");
+	getchar();
+}
